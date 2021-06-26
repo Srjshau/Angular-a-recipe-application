e80d496814dd81a5c17b7c36a86fb8de013c9e8c
srjshau <suraj.shau@gmail.com>
2021-06-26 23:53:07 +0530

    env prod update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'AIzaSyDkn4zkrxE3B-1lbsN-CU1b5WcHbeRT9xs',
+  signupEndPoint: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDkn4zkrxE3B-1lbsN-CU1b5WcHbeRT9xs',
+  signinEndPoint: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDkn4zkrxE3B-1lbsN-CU1b5WcHbeRT9xs'
 };
